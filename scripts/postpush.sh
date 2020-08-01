0136432a3ab4e3fe958c61e72ac2136822b86e9e
olga.sermon <olga.sermon@superawesome.com>
2020-08-01 10:15:43 +0100

    one more line

--- scripts/postpush.sh
+++ scripts/postpush.sh
@@ -1,4 +1,4 @@
 echo "one more line" >> ../README.md
 git add .
 git commit -m"one more line"
-git push https://olga.sermon:PlutoDog00@github.com/olgasermon/recruitment-test.git
+git push https://olgasermon:PlutoDog00@github.com/olgasermon/recruitment-test.git
